e5683602729cc327a4c831accf839a4972173775
PhillMue <muenzuve@gmail.com>
2016-03-13 13:23:12 +0300

    Initial commit

---
+++ app/src/main/java/tubeview/com/tubeview/Config.java
@@ -0,0 +1,12 @@
+package tubeview.com.tubeview;
+
+/**
+ * Created by mue on 3/13/16.
+ */
+public class Config {
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyAHvOBSp-jofzsrMC0WdlX7tpGSw1-V7S4";
+
+}
